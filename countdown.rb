6cabb5cfa17162e6e51eaa73f74c5be019b3b676
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-04-02 05:38:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 #write your code here
 
-def countdown(second_to_midnight)
+def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
 puts "#{seconds_to_midnight} SECOND(S)!"
 seconda_to_midnight -= 1
